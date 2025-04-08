8dcd04cd0d31feae5fcd23039b6c96c0f8b639c7
Brian Caldera <briancaldera07@gmail.com>
2025-04-08 18:55:29 -0400

    agregar data de prueba
    - usar base de datos en memoria para testeos

---
+++ data/dummy.py
@@ -0,0 +1,18 @@
+dummy_proveedores = [{'nombre': 'Proveedor 0 CA', 'telefono': '0123-1234567', 'direccion': 'BNA'},
+               {'nombre': 'Proveedor 1 CA', 'telefono': '0123-1234567', 'direccion': 'BNA'},
+               {'nombre': 'Proveedor 2 CA', 'telefono': '0123-1234567', 'direccion': 'BNA'},
+
+               {'nombre': 'Proveedor 3 CA', 'telefono': '0123-1234567', 'direccion': 'BNA'},
+               {'nombre': 'Proveedor 4 CA', 'telefono': '0123-1234567', 'direccion': 'BNA'},
+               {'nombre': 'Proveedor 5 CA', 'telefono': '0123-1234567', 'direccion': 'BNA'}, ]
+
+dummy_clientes = [{'cedula': 'v12312312', 'nombre': 'cliente 0', 'telefono': '0123-1234567', 'direccion': 'BNA'},
+            {'cedula': 'v12312313', 'nombre': 'cliente 1', 'telefono': '0123-1234567', 'direccion': 'BNA'},
+            {'cedula': 'v12312314', 'nombre': 'cliente 2', 'telefono': '0123-1234567', 'direccion': 'BNA'},
+            {'cedula': 'v12312315', 'nombre': 'cliente 3', 'telefono': '0123-1234567', 'direccion': 'BNA'}, ]
+
+dummy_productos = [
+    {'nombre': 'Producto 0', 'proveedor': 1, 'costo': '2.0', 'precio': '2.5', 'existencia': 10},
+    {'nombre': 'Producto 1', 'proveedor': 1, 'costo': '2.0', 'precio': '2.5', 'existencia': 15},
+    {'nombre': 'Producto 2', 'proveedor': 1, 'costo': '2.0', 'precio': '2.5', 'existencia': 20},
+]
--- db/database.py
+++ db/database.py
@@ -1,5 +1,7 @@
 from peewee import SqliteDatabase as PeeweeSqlite, Database as PeeweeDatabase
 
+use_in_memory = True
+
 
 class Database:
     _instance: PeeweeDatabase | None = None
@@ -7,6 +9,6 @@ class Database:
     @staticmethod
     def get_connection() -> PeeweeDatabase:
         if Database._instance is None:
-            Database._instance = PeeweeSqlite('database.db')
+            Database._instance = PeeweeSqlite(':memory:' if use_in_memory else 'database.db', pragmas={'foreign_keys': 1})
 
         return Database._instance
