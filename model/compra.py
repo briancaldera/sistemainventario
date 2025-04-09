f7fc12b67223fe553d96157145274ba4fa3438a4
Brian Caldera <briancaldera07@gmail.com>
2025-04-09 06:38:09 -0400

    calcular el numero de compra

--- model/compra.py
+++ model/compra.py
@@ -1,19 +1,19 @@
-from peewee import *
-
-from model.producto import Producto
-from model.proveedor import ProveedorAR as Proveedor
-from decimal import Decimal
 from datetime import datetime
+from decimal import Decimal
 from typing import TypedDict
 
+from peewee import *
+
 from db.database import Database as DB
+from model.producto import Producto
+from model.proveedor import ProveedorAR as Proveedor
 
 ItemCompra = TypedDict('ItemCompra', {'producto': Producto, 'cantidad': int, 'precio': Decimal})
 
 
 class Compra(Model):
     compra_id = AutoField()
-    numero_compra = CharField()
+    numero_compra = IntegerField(unique=True)
     proveedor_id = ForeignKeyField(Proveedor, object_id_name='id')
     costo_total = DecimalField()
     fecha = DateField()
@@ -25,15 +25,18 @@ class Compra(Model):
         database = DB.get_connection()
 
     @staticmethod
-    def crear(numero_compra: str, proveedor: Proveedor, costo_total: Decimal,
-              productos: list[ItemCompra]):
+    def crear(proveedor: Proveedor, costo_total: Decimal, productos: list[ItemCompra]):
         # Start transaction here
+
+        max_compra = Compra.select(Compra.numero_compra).order_by(Compra.numero_compra.desc()).limit(1)
+
+        numero_compra = max_compra.get().numero_compra + 1 if max_compra.exists() else 1
+
         conn = DB.get_connection()
         with conn.atomic() as trans:
             compra = Compra.create(numero_compra=numero_compra, proveedor_id=proveedor.id, costo_total=costo_total,
                                    fecha=datetime.now())
             ingresos = []
-
 
             for item in productos:
                 ingreso = Ingreso.crear(compra, item['producto'], item['cantidad'], item['precio'])
@@ -44,8 +47,8 @@ class Compra(Model):
 
 class Ingreso(Model):
     ingreso_id = AutoField()
-    compra_id = ForeignKeyField(Compra,)
-    producto_id = ForeignKeyField(Producto,)
+    compra_id = ForeignKeyField(Compra, )
+    producto_id = ForeignKeyField(Producto, )
     cantidad = IntegerField()
     costo = DecimalField()
 
